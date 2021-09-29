4f1cc4b34c6342b606df68749b6d7091acde559b
Awesome Sauce <jmorenoalan@gmail.com>
2021-09-28 19:34:06 -0700

    Matrice

--- flop.py
+++ flop.py
@@ -1,4 +1,4 @@
-class Parent():
+class flop():
     def root(sqrNum):
             squareroot = sqrNum ** 0.5
             return squareroot
@@ -35,5 +35,32 @@ class Parent():
         return (3.14159 * (radius**2)) * height/3
     def volume_sphere(radius):
         return ((4/3*3.14159) * (radius**3))
-volume = Parent.volume_sphere(4)
-print(volume)
+
+    def matrix(*args):
+        # Matrix MAX SIZE == 3x3
+        Matrix = [[args[0], args[1], args[2]], 
+                 [args[3], args[4], args[5]],
+                 [args[6], args[7], args[8]],]
+        return Matrix
+test = flop.matrix(1, 2, 3, 4, 5 ,6 ,7 ,8 ,9 ,0)
+print(test[1][2])
+
+#print(flop.order_descending(test))
+
+
+
+'''
+
+[1, 2, 3, 2, 3, 4, 3, 4, 5]
+
+A = [[1, 4, 5, 12], 
+    [-5, 8, 9, 0],
+    [-6, 7, 11, 19],
+    [2, 5, 6]]
+
+print("A =", A) 
+print("A[1] =", A[1])      # 2nd row
+print("A[1][2] =", A[2][2])   # 3rd element of 2nd row
+print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
+
+'''
