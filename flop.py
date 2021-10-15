8f9e66bb0ecfdfcb4d04aad7e3913b91ec756ed8
Awesome Sauce <jmorenoalan@gmail.com>
2021-10-15 07:07:59 -0700

    Order Matrice

--- flop.py
+++ flop.py
@@ -1,20 +1,38 @@
 #############################
 ### Flop Settings Manager ###
 #############################
+'''
+Manages Global variables
+Changes them
+And can declare them
+'''
+from typing import Type
+
+
 class Settings:
-    def __init__(self, StartUp, pieDefine):
+    def __init__(self, StartUp, pieDefine, fancyShow=True):
         self.startupState = StartUp
         self.pi = pieDefine
+        self.fancy = fancyShow
     def startup(self):
             global PI
+            global FS
             PI = self.pi
+            FS = self.fancy
     def changeCurrent(self):
         global PI
+        global FS
         PI = self.pi
+        FS = self.fancy
 
 #############################
 ### Main Section of Flop ####
 #############################
+'''
+Parent Class of Flop!
+contains matrix, math and will have much more added!
+
+'''
 class Parent():
     class matrix():
         def __init__(self, *args):
@@ -23,8 +41,13 @@ class Parent():
                             [self.args[3], self.args[4], self.args[5]],
                             [self.args[6], self.args[7], self.args[8]],]
         def fancy(self):
-            print(str(self.Matrix[0]) + "\n" + str(self.Matrix[1]) + "\n" + str(self.Matrix[2]))
-            return self.Matrix
+            if FS == True:
+                print(str(self.Matrix[0]) + "\n" + str(self.Matrix[1]) + "\n" + str(self.Matrix[2]))
+                return self.Matrix
+            elif FS == False:
+                return self.Matrix
+            else:
+                raise TypeError("Please Enter a Boolean Value!")
     class math():        
         def exponents(num, power):
             return num ** power
@@ -41,22 +64,38 @@ class Parent():
                     return intA == intB
 
         def order_descending(*args):
-            list_args = []
-            for index in range(len(args)):
-                if type(args[index]) == list:
-                    list_args.extend(args[index])
+            for iterate in range(len(args)):
+                if type(args[iterate]) == type(Parent.matrix(0,0,0,0,0,0,0,0,0)):
+                    matrice_args_final = args[iterate].fancy()
+                    matrice_args = []
+                    for matrice_index in range(len(matrice_args_final)):
+                            matrice_args.extend(matrice_args_final[matrice_index])
+                    return sorted(matrice_args, reverse=True)
                 else:
-                    list_args.append(args[index])
-            return sorted(list_args, reverse=True)
+                    list_args = []
+                    for index in range(len(args)):
+                        if type(args[index]) == list:
+                            list_args.extend(args[index])
+                        else:
+                            list_args.append(args[index])
+                    return sorted(list_args, reverse=True)
 
         def order_ascending(*args):
-            list_args = []
-            for index in range(len(args)):
-                if type(args[index]) == list:
-                    list_args.extend(args[index])
+            for iterate in range(len(args)):
+                if type(args[iterate]) == type(Parent.matrix(0,0,0,0,0,0,0,0,0)):
+                    matrice_args_final = args[iterate].fancy()
+                    matrice_args = []
+                    for matrice_index in range(len(matrice_args_final)):
+                            matrice_args.extend(matrice_args_final[matrice_index])
+                    return sorted(matrice_args, reverse=False)
                 else:
-                    list_args.append(args[index])
-            return sorted(list_args, reverse=False)
+                    list_args = []
+                    for index in range(len(args)):
+                        if type(args[index]) == list:
+                            list_args.extend(args[index])
+                        else:
+                            list_args.append(args[index])
+                    return sorted(list_args, reverse=False)
             
         def volume_cylinder(radius, height):
             return (PI * (radius**2)) * height
@@ -65,18 +104,17 @@ class Parent():
         def volume_sphere(radius):
             return ((4/3*PI) * (radius**3))
 #############################
-### Search Section of Flop ##
+### Search Engine of Flop ###
 #############################
-# function as class
+'''
+Flop Search engine
+allows search inside matrice!
+and normal lists aswell.
+helps shorten syntax when doing searches!
+'''
 class Search(Parent):
     def integer(int, lookfor):
-        if type(int) == list:
-            for iterate in range(len(int)):
-                if lookfor in int[iterate]:
-                    return True
-                else:
-                    return False
-        elif type(int) == type(1):
+        if type(int) == type(1):
             while (int>0):
                 n = int%10
                 int = int//10
@@ -85,5 +123,19 @@ class Search(Parent):
                     return True         
             return False
         else:
-            print("Flop currently only has support for type int and list!")
-
+            raise ValueError("Enter a Integer!")
+    def list(Floplst, lookfor):
+        if type(Floplst) == list:
+            for iterate in range(len(Floplst)):
+                if lookfor in Floplst[iterate]:
+                    return True
+        else:
+            raise ValueError("Enter a List!")
+    def string(string, lookfor):
+        if type(string) == str:
+                if lookfor in string:
+                    return True
+                else:
+                    return False
+        else:
+            raise ValueError("Enter a String!")
--- test.py
+++
@@ -1,4 +0,0 @@
-from flop import Settings, Parent
-
-Settings(1, 3.14).startup()
-print(Parent.math.volume_cylinder(10, 10))
